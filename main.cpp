1c392837dda088174d0d619902f4a603e457f5a2
Viktor Zetterström <viktorzetterstrom@gmail.com>
2014-03-02 14:41:49 +0100

    La till saker
    Alko

--- main.cpp
+++ main.cpp
@@ -8,6 +8,12 @@ int main()
   printhello();
 
   std::cout << "Two times five is: " << my_multiply(2,5) << std::endl;
+  
+  if (my_multiply(2,5) > 1)
+  {
+	  std::cout << "It's larger then 1" << std::endl;
+  }
+
 
   std::cin.ignore();
   return 0;
