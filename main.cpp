464a31cf27b8bd96a2cea52008980f14d266bd62
Andreas Samuelsson <andreassamuelsson87@gmaol.com>
2014-03-02 13:10:00 +0100

    huvudfilen

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+void printhello();
+
+int main()
+{
+  printhello();
+
+  std::cin.ignore();
+  return 0;
+}
+
+void printhello()
+{
+  std::cout << "Ich habe alcohol getränken!" << std::endl;
+}
